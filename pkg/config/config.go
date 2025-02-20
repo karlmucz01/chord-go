36ba16709d193b18bbbc4c33b02149a084089f31
kmucz01 <kmucz01@terpmail.umd.edu>
2025-02-20 00:02:56 -0500

    Updated uint64 to uint32 to be more manageable

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,4 +3,7 @@ package config
 import "math"
 
 const BasicIdentifierSpaceMax = math.MaxUint16
-const IdentifierSpaceMax = math.MaxUint64
+const BasicIdentifierScale = 16
+
+const IdentifierSpaceMax = math.MaxUint32
+const IdentifierScale = 32
--- pkg/utils/hashing.go
+++ pkg/utils/hashing.go
@@ -20,42 +20,42 @@ func HashStringSha1(input string) []byte {
 	return hashStr[:8]
 }
 
-func ByteSliceToUInt64(hashSlice []byte) (uint64, error) {
-	// Converts a []byte slice to an Uint64 integer
+func ByteSliceToUint32(hashSlice []byte) (uint32, error) {
+	// Converts a []byte slice to an Uint32 integer
 
-	if len(hashSlice) != 8 {
-		return 0, errors.New("byte slice must be of length 8")
+	if len(hashSlice) != 4 {
+		return 0, errors.New("byte slice must be of length 4")
 	}
 
-	hashUInt64 := uint64(0)
-	for i := 0; i < 8; i++ {
+	hashUint32 := uint32(0)
+	for i := 0; i < 4; i++ {
 		// set the bytes at each 8 bit interval with a byte from the slice
-		hashUInt64 = (hashUInt64 << 8) | uint64(hashSlice[i])
+		hashUint32 = (hashUint32 << 8) | uint32(hashSlice[i])
 	}
-	return hashUInt64, nil
+	return hashUint32, nil
 }
 
 func ByteSliceToUint16(hashSlice []byte) (uint16, error) {
 	// Converts a []byte slice to an Uint16 integer
 
-	if len(hashSlice) != 8 {
-		return 0, errors.New("byte slice must be of length 8")
+	if len(hashSlice) != 2 {
+		return 0, errors.New("byte slice must be of length 2")
 	}
 
 	hashUint16 := uint16(0)
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 2; i++ {
 		// set the bytes at each 8 bit interval with a byte from the slice
 		hashUint16 = (hashUint16 << 8) | uint16(hashSlice[i])
 	}
 	return hashUint16, nil
 }
 
-func IdentifierFromStringSha256(input string) uint64 {
+func IdentifierFromStringSha256(input string) uint32 {
 	// General purpose function for passing a string and getting an identifier for the
 	// identifier space
 	inputHashSlice := HashStringSha256(input)           // guaranteed to return a slice of length 8
-	inputUInt64, _ := ByteSliceToUInt64(inputHashSlice) // no need to capture error
-	return inputUInt64 % config.IdentifierSpaceMax
+	inputUint32, _ := ByteSliceToUint32(inputHashSlice) // no need to capture error
+	return inputUint32 % config.IdentifierSpaceMax
 }
 
 func IdentifierFromStringSha1(input string) uint16 {
